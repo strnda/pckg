c59365fd2705f9327f972024acb46f3d7818eee7
strnda <strnadf@fzp.czu.cz>
2024-05-09 11:18:45 +0200

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,28 @@
+#' hello world expamle 2.0
+#'
+#' liuSHHFSDIUBEKJvnhfbFDVNSDCBHSDBJDSC
+#' JKSBDchjBSDJvNHCVGC
+#'
+#'
+#'
+#' jhbfchjndskjnmkdnvbcbsdhcbjksbljhcb
+#' slkmxc
+#'
+#'
+#'
+#' @param x value to be printed
+#'
+#' @export
+#'
+#' @examples
+#'
+#' hello()
+#'
+hello <- function(x = NULL) {
+
+  if (is.null(x = x)) {
+    print("Hello, world!")
+  } else {
+    print(x = x)
+  }
+}
